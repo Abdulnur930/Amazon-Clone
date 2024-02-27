85195feed60e920e0f4086dda3da22297b212552
Abdulnur930 <hussenabdulnur@gmail.com>
2024-02-27 23:33:31 +0300

    carousel and category

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import CarouselEffect from './Component/Carousel/CarouselEffect';
+import Category from './Component/Category/Category';
 import Header from './Component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <CarouselEffect/>
+      <Category/>
     </div>
   );
 }
