b32da806320b0ca980cb9c1dd54eb70757142309
Abdulnur930 <hussenabdulnur@gmail.com>
2024-03-06 23:15:15 +0300

    deploy

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios"
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-866b0/us-central1/api",
+  // local instance firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-866b0/us-central1/api",
+  // depolyed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-g121.onrender.com/",
 });
 export { axiosInstance };
