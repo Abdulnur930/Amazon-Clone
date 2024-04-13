d25e92f116b08baf63c95166001e2bb51d0ee339
Abdulnur930 <hussenabdulnur@gmail.com>
2024-04-13 09:41:30 +0300

    backend

--- amazon-clone/src/Api/axios.js
+++ amazon-clone/src/Api/axios.js
@@ -4,5 +4,7 @@ const axiosInstance = axios.create({
   // baseURL: "http://127.0.0.1:5001/clone-866b0/us-central1/api",
   // depolyed version of amazon server on render.com
   baseURL: "https://amazon-api-deploy-g121.onrender.com/",
+  //local
+  //baseURL: "http://localhost:1000
 });
 export { axiosInstance };
